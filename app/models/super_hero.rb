d26e54a10ead76610156a386372877a7869e8654
Jesse Wenger <wengerj@gmail.com>
2021-04-07 12:30:09 +0000

    Automatically backed up by Learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -1,2 +1,16 @@
 class SuperHero
+  attr_accessor :name, :power, :biography
+  
+  @@all = []
+  
+  def initialize(details)
+    @name = name
+    @power = power
+    @biography = biography
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
 end
